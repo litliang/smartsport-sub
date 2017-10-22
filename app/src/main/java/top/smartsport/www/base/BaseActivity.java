50bdaad75f8863312b86f684768442cf3c129221
skygst <sky0719zx@163.com>
2017-10-22 21:27:35 +0800

    抽取activity权限;Imageutil bug修复；

--- app/src/main/java/top/smartsport/www/base/BaseActivity.java
+++ app/src/main/java/top/smartsport/www/base/BaseActivity.java
@@ -1,21 +1,11 @@
 package top.smartsport.www.base;
 
-import android.Manifest;
 import android.app.ActionBar;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -26,37 +16,25 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import app.base.ActivityPool;
-import app.base.SPrefUtil;
-import app.base.framework.CrashHandler;
-import app.base.widget.HttpFrameLayout;
-import cn.jiguang.share.android.api.Platform;
-import intf.JsonUtil;
-
-import com.zhy.autolayout.AutoLayoutActivity;
-
-import app.base.action.ViewInflater;
-import app.base.framework.Init;
-import cn.jiguang.share.android.api.ShareParams;
-import intf.FunCallback;
-
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 import org.xutils.x;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
-import intf.MapBuilder;
+import app.base.ActivityPool;
+import app.base.action.ViewInflater;
+import app.base.widget.HttpFrameLayout;
+import cn.jiguang.share.android.api.Platform;
+import cn.jiguang.share.android.api.ShareParams;
+import intf.FunCallback;
+import intf.JsonUtil;
 import top.smartsport.www.R;
 import top.smartsport.www.actions.Fav;
 import top.smartsport.www.bean.NetEntity;
 import top.smartsport.www.bean.RegInfo;
 import top.smartsport.www.bean.TokenInfo;
 import top.smartsport.www.dialog.CustomProgressDialog;
-import top.smartsport.www.listener.OnClickThrottleListener;
 import top.smartsport.www.utils.ActivityStack;
 import top.smartsport.www.xutils3.MyCallBack;
 import top.smartsport.www.xutils3.X;
@@ -65,7 +43,7 @@ import top.smartsport.www.xutils3.X;
  * Created by Aaron on 2017/6/30.
  */
 
-public abstract class BaseActivity extends AutoLayoutActivity {
+public abstract class BaseActivity extends Permissionactivity {
 
     public String initData;
 
@@ -588,139 +566,9 @@ public abstract class BaseActivity extends AutoLayoutActivity {
         return obj;
     }
 
-    /**
-     * 需要进行检测的权限数组
-     */
-    protected String[] needPermissions = {
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.READ_PHONE_STATE,
-            Manifest.permission.CAMERA
-
-
-    };
-
-    private static final int PERMISSON_REQUESTCODE = 0;
-
-    /**
-     * 判断是否需要检测，防止不停的弹框
-     */
-    private boolean isNeedCheck = true;
-
     @Override
     protected void onResume() {
         super.onResume();
-        if (isNeedCheck) {
-            checkPermissions(needPermissions);
-        }
-    }
-
-    /**
-     * @param needRequestPermissonList
-     * @since 2.5.0
-     */
-    private void checkPermissions(String... permissions) {
-        List<String> needRequestPermissonList = findDeniedPermissions(permissions);
-        if (null != needRequestPermissonList
-                && needRequestPermissonList.size() > 0) {
-            ActivityCompat.requestPermissions(this,
-                    needRequestPermissonList.toArray(
-                            new String[needRequestPermissonList.size()]),
-                    PERMISSON_REQUESTCODE);
-        }
-    }
-
-    /**
-     * 获取权限集中需要申请权限的列表
-     *
-     * @param permissions
-     * @return
-     * @since 2.5.0
-     */
-    private List<String> findDeniedPermissions(String[] permissions) {
-        List<String> needRequestPermissonList = new ArrayList<String>();
-        for (String perm : permissions) {
-            if (ContextCompat.checkSelfPermission(this,
-                    perm) != PackageManager.PERMISSION_GRANTED
-                    || ActivityCompat.shouldShowRequestPermissionRationale(
-                    this, perm)) {
-                needRequestPermissonList.add(perm);
-            }
-        }
-        return needRequestPermissonList;
-    }
-
-    /**
-     * 检测是否说有的权限都已经授权
-     *
-     * @param grantResults
-     * @return
-     * @since 2.5.0
-     */
-    private boolean verifyPermissions(int[] grantResults) {
-        for (int result : grantResults) {
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] paramArrayOfInt) {
-        if (requestCode == PERMISSON_REQUESTCODE) {
-            if (!verifyPermissions(paramArrayOfInt)) {
-                showMissingPermissionDialog();
-                isNeedCheck = false;
-            }
-        }
-    }
-
-    /**
-     * 显示提示信息
-     *
-     * @since 2.5.0
-     */
-    private void showMissingPermissionDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.notifyTitle);
-        builder.setMessage(R.string.notifyMsg);
-
-        // 拒绝, 退出应用
-        builder.setNegativeButton(R.string.cancel,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
-
-        builder.setPositiveButton(R.string.setting,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startAppSettings();
-                    }
-                });
-
-        builder.setCancelable(false);
-
-        builder.show();
-    }
-
-    /**
-     * 启动应用的设置
-     *
-     * @since 2.5.0
-     */
-    private void startAppSettings() {
-        Intent intent = new Intent(
-                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.setData(Uri.parse("package:" + getPackageName()));
-        startActivity(intent);
     }
 
     @Override
